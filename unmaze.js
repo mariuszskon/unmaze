5c822863f5ec75be7b299564f9d9740ce709093b
Mariusz Skoneczko <mariuszskon@users.noreply.github.com>
2017-03-18 14:08:27 +1100

    Added Maze class

--- unmaze.js
+++ unmaze.js
@@ -3,3 +3,39 @@
    Licensed under the MIT license
 */
 
+const MAZE = {
+    FREE: 0,
+    WALL: 1
+
+};
+
+class Maze {
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+
+        this.maze = new Array(width);
+        // create 'matrix' maze where maze[x][y] such that x < width, y < height
+        for (let i = 0; i < width; i++) {
+            this.maze[i] = new Array(height);
+        }
+        this.allWall();
+
+        this.start = {x: null, y: null};
+        this.end = {x: null, y: null};
+        this.robot = {x: null, y: null};
+    }
+
+    allWall() {
+        for (let i = 0; i < this.width; i++) {
+            for (let j = 0; j < this.height; j++) {
+                this.maze[i][j] = {type: MAZE.WALL};
+            }
+        }
+    }
+
+    robotToStart() {
+        this.robot.x = this.start.x;
+        this.robot.y = this.start.y;
+    }
+}
